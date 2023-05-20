51331dc4c40486e96a7b0082343c9c1402fe64c1
shBLOCK <3332908658@qq.com>
2023-05-21 06:49:19 +0800

    Fixed coordinate axis lag

--- utils/coordinate_axis.py
+++ utils/coordinate_axis.py
@@ -14,11 +14,14 @@ def _label(draw_list: imgui.ImDrawList, num: float, pos: Vector2, align: Vector2
     pos += extra_offset * align
     draw_list.add_text((*pos,), col, text)
 
+def _getMinDistToBound(value, small, big):
+    return min(abs(small - value), abs(value - big))
+
 # noinspection PyTypeChecker
 def drawCoordinateAxis(translation: Vector2, scale: float, line_col: Color, text_col: Color):
     # TODO: why bg draw list doesn't work when window is small??
-    draw_list = imgui.get_background_draw_list()
     viewport = imgui.get_main_viewport()
+    draw_list = imgui.get_background_draw_list(viewport)
     pos = Vector2(viewport.pos)
     size = Vector2(viewport.size)
     pos1 = pos + size
@@ -44,6 +47,7 @@ def drawCoordinateAxis(translation: Vector2, scale: float, line_col: Color, text
     grid_pix = grid / scale * size.y
 
     i = 1
+    draws = 0
     label_size = 8
     while True:
         num = grid * i
@@ -53,8 +57,29 @@ def drawCoordinateAxis(translation: Vector2, scale: float, line_col: Color, text
         # y is reversed
         yn = origin.y + off
         yp = origin.y - off
-        if xn < pos.x and xp > pos1.x and yn > pos1.y and yp < pos.y:
+
+        gap = 40
+        g_pos = pos + Vector2(-gap, -gap)
+        g_pos1 = pos1 + Vector2(+gap, +gap)
+        drawing_xn = g_pos.x < xn < g_pos1.x
+        drawing_xp = g_pos.x < xp < g_pos1.x
+        drawing_yn = g_pos.y < yn < g_pos1.y
+        drawing_yp = g_pos.y < yp < g_pos1.y
+
+        if xn <= g_pos.x and xp >= g_pos1.x and yn >= g_pos1.y and yp <= g_pos.y:
             break
+
+        if not (drawing_xn or drawing_xp or drawing_yn or drawing_yp):
+            min_dist = min(
+                _getMinDistToBound(xn, g_pos.x, g_pos1.x) if xn > g_pos.x else math.inf,
+                _getMinDistToBound(xp, g_pos.x, g_pos1.x) if xp < g_pos1.x else math.inf,
+                _getMinDistToBound(yn, g_pos.y, g_pos1.y) if yn < g_pos1.y else math.inf,
+                _getMinDistToBound(yp, g_pos.y, g_pos1.y) if yp > g_pos.y else math.inf,
+            )
+            increment = int(min_dist / grid_pix)
+            if increment > 0:
+                i += increment
+                continue
 
         x_axis_align = -1 if (x_axis_pos < size.y / 2 + pos.y) else 1
         x_axis_align_text = x_axis_align
@@ -70,16 +95,21 @@ def drawCoordinateAxis(translation: Vector2, scale: float, line_col: Color, text
             y_axis_align_text *= -1
             y_text_extra_offset = label_size
 
-        draw_list.add_line((xp, x_axis_pos), (xp, x_axis_pos - label_size * x_axis_align), line_col, 1)
-        _label(draw_list, num, (xp, x_axis_pos), Vector2(0, x_axis_align_text), x_text_extra_offset, text_col)
+        if drawing_xp:
+            draw_list.add_line((xp, x_axis_pos), (xp, x_axis_pos - label_size * x_axis_align), line_col, 1)
+            _label(draw_list, num, (xp, x_axis_pos), Vector2(0, x_axis_align_text), x_text_extra_offset, text_col)
 
-        draw_list.add_line((xn, x_axis_pos), (xn, x_axis_pos - label_size * x_axis_align), line_col, 1)
-        _label(draw_list, -num, (xn, x_axis_pos), Vector2(0, x_axis_align_text), x_text_extra_offset, text_col)
+        if drawing_xn:
+            draw_list.add_line((xn, x_axis_pos), (xn, x_axis_pos - label_size * x_axis_align), line_col, 1)
+            _label(draw_list, -num, (xn, x_axis_pos), Vector2(0, x_axis_align_text), x_text_extra_offset, text_col)
 
-        draw_list.add_line((y_axis_pos, yp), (y_axis_pos - label_size * y_axis_align, yp), line_col, 1)
-        _label(draw_list, num, (y_axis_pos, yp), Vector2(y_axis_align_text, 0), y_text_extra_offset, text_col)
+        if drawing_yp:
+            draw_list.add_line((y_axis_pos, yp), (y_axis_pos - label_size * y_axis_align, yp), line_col, 1)
+            _label(draw_list, num, (y_axis_pos, yp), Vector2(y_axis_align_text, 0), y_text_extra_offset, text_col)
 
-        draw_list.add_line((y_axis_pos, yn), (y_axis_pos - label_size * y_axis_align, yn), line_col, 1)
-        _label(draw_list, -num, (y_axis_pos, yn), Vector2(y_axis_align_text, 0), y_text_extra_offset, text_col)
+        if drawing_yn:
+            draw_list.add_line((y_axis_pos, yn), (y_axis_pos - label_size * y_axis_align, yn), line_col, 1)
+            _label(draw_list, -num, (y_axis_pos, yn), Vector2(y_axis_align_text, 0), y_text_extra_offset, text_col)
 
         i += 1
+        draws += 1
